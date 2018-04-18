81e0a453c8eb14089af1c900b6c4ebde92cc0be1
Shinya Maeda <shinya@gitlab.com>
2018-04-18 11:40:07 +0900

    ok

--- gitlab/gdk_up.sh
+++ gitlab/gdk_up.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-source ~/.bash_profile
+#source ~/.bash_aliases
+WORKSPACE="/home/shinya/workspace"
+CE_REPO="$WORKSPACE/gdk-ce"
+EE_REPO="$WORKSPACE/gdk-ee"
+
 set -e # Fail -> Exit
 
 echo "Which do you want to set up? 1) CE 2) EE 3) GEO"
@@ -9,11 +13,11 @@ read target
 case $target in
 
     1)
-        goce
+        cd $CE_REPO
 	edition=ce
         ;;
     2)
-        goee
+        cd $EE_REPO
 	edition=ee
         ;;
     3)
@@ -27,28 +31,28 @@ case $target in
 esac
 
 echo "Updating GDK..."
-g feo
-g co master
-g plo master
+git fetch origin
+git checkout master
+git pull origin master
 
 echo "Updating Source..."
 cd gitlab
-g feo
-g co master
-g plo master
+git fetch origin
+git checkout master
+git pull origin master
 
 echo "gdk reconfigureing..."
 cd ../
 gdk reconfigure
 
 echo "gdk set_ip... edition is $edition"
-g co feature/sm/add-utility-set-ip
-g plo feature/sm/add-utility-set-ip
-g merge master
+git checkout feature/sm/add-utility-set-ip
+git pull origin feature/sm/add-utility-set-ip
+git merge master
 gdk set_ip $edition
 
 echo "gdk updating and running..."
-g co master
+git checkout master
 gdk run db &
 (sleep 5 && gdk update && gdk run app) &
 wait
